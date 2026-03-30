e1ff41a95d1d8873e29c16f80da8b511fcfbea4e
Anuj Ghimire <anujghimire41@gmail.com>
2026-03-30 18:23:14 +0545

    adding code inside fetch user data app

--- fetching user data using useEffect hook/src/App.jsx
+++ fetching user data using useEffect hook/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
+  const [data, setdata] = useState([]);
+  const [load, setload] = useState(false)
+  useEffect(() => {
+    setdata = fetch("https://jsonplaceholder.typicode.com/users").json();
+  }, [load]);
 
   return (
     <>
-      
+      <button
+        onClick={() => {
+          setload(true);
+        }}
+      >
+        Load Users
+      </button>
+
+      <ol>
+        {data.map()}
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
