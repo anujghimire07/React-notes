0421bc7bbf751aedc5c5abd8272f8a5076d20cb6
Anuj Ghimire <anujghimire41@gmail.com>
2026-04-01 22:07:35 +0545

    conditional rendering completed

--- Conditional rendering and rendering lists/src/App.jsx
+++ Conditional rendering and rendering lists/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  const [showbtn, setshowbtn] = useState(false);
+ 
 
+  /* function Todo() { //similar to App() function
+    return (<><div>i am todo</div></>);
+  }
+    */
   return (
     <>
-   
+      {/*  <Todo></Todo> */}
+      <button
+        onClick={() => {
+          setshowbtn(!showbtn); //if true => false;  if false => true
+        }}
+      >
+        show
+      </button>
+      <div>
+        {/*suitable for one expected outcome*/}
+        {showbtn && <p>i will be shown if show button is clicked </p>}
+
+        {/* {showbtn? <p>show button was clicked</p>: <p>nothing</p>} */}
+      </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
