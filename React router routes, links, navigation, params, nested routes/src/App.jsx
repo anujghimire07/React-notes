01b53d7b51c25a994b6180d25ff5ea82c90f4e8a
Anuj Ghimire <anujghimire41@gmail.com>
2026-04-03 23:41:36 +0545

    created an object from createBrowserRouter in App.jsx

--- React router routes, links, navigation, params, nested routes/src/App.jsx
+++ React router routes, links, navigation, params, nested routes/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState, useEffect } from "react";
-
 import "./App.css";
 import Nav from "./Navbar";
+import { createBrowserRouter } from "react-router-dom";
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element:""
+    }, {}, 
+  ]);
   return (
     <>
-      <Nav></Nav>
-    </> 
+    <div> this is a paragraph</div>
+      <Nav />
+    </>
   );
 }
 
--- React router routes, links, navigation, params, nested routes/src/Navbar.jsx
+++ React router routes, links, navigation, params, nested routes/src/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {createBrowserRouter} from 'react-router-dom'
+
 
 export default function Nav() {
+ 
   return (
     <>
       <nav>
