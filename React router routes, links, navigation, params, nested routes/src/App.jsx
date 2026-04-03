891b1db74c682e93e505173d6a70d60ef5c985ac
Anuj Ghimire <anujghimire41@gmail.com>
2026-04-03 23:45:05 +0545

    linked Home.jsx to App.jsx

--- React router routes, links, navigation, params, nested routes/src/App.jsx
+++ React router routes, links, navigation, params, nested routes/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Nav from "./Navbar";
+import Home from "./Home";
 import { createBrowserRouter } from "react-router-dom";
 function App() {
   let router = createBrowserRouter([
     {
       path: "/",
-      element:""
-    }, {}, 
+      element: <Home/>,
+    },
+    {},
   ]);
   return (
     <>
-    <div> this is a paragraph</div>
+      <div> this is a paragraph</div>
       <Nav />
     </>
   );
