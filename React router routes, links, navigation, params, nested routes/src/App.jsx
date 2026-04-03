2c64b9c6216ef38f4f5e02b1e27caf4c9180a179
Anuj Ghimire <anujghimire41@gmail.com>
2026-04-03 23:49:45 +0545

    created Login.jsx and imported it to App.jsx iside createBrowserRouter

--- React router routes, links, navigation, params, nested routes/src/App.jsx
+++ React router routes, links, navigation, params, nested routes/src/App.jsx
@@ -2,15 +2,20 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Nav from "./Navbar";
 import Home from "./Home";
+import Login from "./Login";
 import { createBrowserRouter } from "react-router-dom";
 function App() {
   let router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>,
+      element: <Home />,
     },
-    {},
+    {
+      path: "/login",
+      element: <Login />,
+    },
   ]);
+  s;
   return (
     <>
       <div> this is a paragraph</div>
---
+++ React router routes, links, navigation, params, nested routes/src/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      this is page called login
+    </div>
+  )
+}
+
+export default Login
