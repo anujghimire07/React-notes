4487d4e2c148dc48ea7ece9dceda1ae9cade1b7a
Anuj Ghimire <anujghimire41@gmail.com>
2026-04-11 14:47:36 +0545

    useCallback hook completed

--- useCallback Hook/src/App.jsx
+++ useCallback Hook/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
+import { useState, useCallback } from "react";
 
-import './App.css'
-import Navbar from './Components/Navbar'
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
- 
+  const [count, setcount] = useState(0);
+  const [message, setmessage] = useState("good")
+const getMessage = useCallback(() => {
+  return "another" +count
+}
+, [/*count */])//changes when count changes ie. count is the dependency
+
+
 
   return (
     <>
-      <Navbar value={"good"}/>
-                
+      <Navbar value={"good"} send={getMessage} />
+      <div>
+        <button
+          onClick={() => {
+            setcount(count + 1);
+          }}
+        >
+          count is {count}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- useCallback Hook/src/Components/Navbar.jsx
+++ useCallback Hook/src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { memo } from "react";
 
-const Navbar = ({value}) => {
+const Navbar = ({ value, send }) => {
+  console.log("navbar is rendered");
   return (
     <div>
       <p>i am a {value} navbar</p>
+      <button onClick={()=>{getMessage()}}>message</button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default memo(Navbar);
